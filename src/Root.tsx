06bc5fe23053d58ccf2ff9f46096720c0f07c4cc
Shagen A <asriyan.sh@gmail.com>
2022-01-11 11:00:35 +0300

    migrate to CRA

---
+++ src/Root.tsx
@@ -0,0 +1,13 @@
+import './Root.css';
+import Header from './Header';
+
+const Root = () => {
+  return (
+    <div className="root">
+      <Header />
+      {/* <BackFrame /> */}
+    </div>
+  );
+}
+
+export default Root;
